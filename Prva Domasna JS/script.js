c32947d184fd27eb073811bb2e0878393e3ddf0d
Alek986 <149313522+Alek986@users.noreply.github.com>
2023-12-08 17:47:11 +0100

    Prva domasna JS

---
+++ Prva Domasna JS/script.js
@@ -0,0 +1,18 @@
+alert ("First HOmework JS")
+
+let phone = 119.95;
+let phones = 30 * phone;
+let taxPhone = phone * 0.5;
+let taxPhones = taxPhone * 30;
+let totalPrice = phones + taxPhones;
+console.log ("The price is: " + totalPrice);
+console.log(typeof(totalPrice))
+
+
+let priceApple = 119.95
+let apple = prompt("Enter number of Iphones")
+let parseApple = parseInt(apple) * priceApple
+let taxApple = parseApple * 0.5
+let price = parseApple + taxApple
+console.log("The prise is: " + price)
+console.log(typeof(price))
