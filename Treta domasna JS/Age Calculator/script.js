f0b66941259bf779cb5c765b93803385297e345e
Alek986 <149313522+Alek986@users.noreply.github.com>
2023-12-18 00:04:10 +0100

    Treta domasna

---
+++ Treta domasna JS/Age Calculator/script.js
@@ -0,0 +1,9 @@
+function calculateAge(birthYear, currentYear){
+let age = currentYear - birthYear
+console.log(`You are ${age} years old`)
+return age
+}
+
+calculateAge(1986, 2023)
+calculateAge(1990, 2023)
+calculateAge(1980, 2023)
