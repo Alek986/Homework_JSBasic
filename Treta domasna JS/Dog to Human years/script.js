e74c4b4e26f25ade30a6984261ed19def2cf53c5
Alek986 <149313522+Alek986@users.noreply.github.com>
2023-12-17 21:23:02 +0100

    Treta domasna

--- Treta domasna JS/Dog to Human years/script.js
+++ Treta domasna JS/Dog to Human years/script.js
@@ -1,8 +1,19 @@
 function calculateDogYears(humanYears){
     let dogYears = 7 * humanYears
+    console.log(dogYears)
     return dogYears
+        
 }
 
-let dogAge = calculateDogYears(37)
+calculateDogYears(37)
 
+
+
+function calculateDogYear(humanYear){
+    let dogYear = 7 * humanYear
+        return dogYear
+        
+}
+
+let dogAge = calculateDogYear(37)
 console.log(dogAge)
