11cf7b4cb2f39942e6e33601bf94e20c16de9c80
Alek986 <149313522+Alek986@users.noreply.github.com>
2023-12-18 01:27:09 +0100

    Treta domasna

--- Treta domasna JS/ATM/script.js
+++ Treta domasna JS/ATM/script.js
@@ -1,11 +1,14 @@
-let withdrawalAmmount = parseInt(prompt("Enter your withdrawal amount"))
+let withdrawalAmmount = parseInt(prompt("Enter your withdrawal ammount"))
 let accountBalance = 1000
 function atmWithdrawal(accountBalance, withdrawalAmmount){
     if (withdrawalAmmount > accountBalance){
         console.log("Not enough money")
     }
     else if (withdrawalAmmount -= accountBalance){
-        console.log(`Withdrawal successful! Remaining balance: ${????}`)
+        console.log(`Withdrawal successful! Remaining balance: ${accountBalance}`)
     }
 }
+let result = accountBalance - withdrawalAmmount
+console.log(atmWithdrawal(accountBalance, withdrawalAmmount))
 
+// Ne mi teknuva kako da pokaze uste kolku ammount kje ima na smetka otkako kje povlece pari "ona sto e prikazano vo 8 linija" 
